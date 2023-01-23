9415393b78a5db4e3aef10dbe15e2e8be0761824
cvcami <123136539+cvcami@users.noreply.github.com>
2023-01-23 16:22:55 -0300

    Add files via upload
    CV camila viva proyecto final

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("button").addEventListener("click", function(){
+console.log("caracteristicas personales");
+document.getElementById("demo").innerHTML= "creativo,responsable,maduro y puntual"
+
+
+
+
+
+
+
+})
